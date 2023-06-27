37903e14bafeee26bacaf1b4120344118609b5a5
fadthegreat <fad16papa@gmail.com>
2023-06-27 22:44:16 +0800

    Commit changes

---
+++ Bloggie.Web/Repositories/Interfaces/IImagesInterface.cs
@@ -0,0 +1,7 @@
+﻿namespace Bloggie.Web.Repositories.Interfaces
+{
+    public interface IImagesInterface
+    {
+        Task<string> UploadAsync(IFormFile formFile);
+    }
+}
---
+++ Bloggie.Web/Repositories/Services/ImagesService.cs
@@ -0,0 +1,12 @@
+﻿using Bloggie.Web.Repositories.Interfaces;
+
+namespace Bloggie.Web.Repositories.Services
+{
+    public class ImagesService : IImagesInterface
+    {
+        public Task<string> UploadAsync(IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
