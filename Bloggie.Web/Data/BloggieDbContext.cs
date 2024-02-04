f3f545e21be778a9da8ab1e60789f8fd51790429
fad16papa <fad16papa@gmail.com>
2024-02-04 16:47:23 +0800

    Commit changes

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -13,5 +13,16 @@ namespace Bloggie.Web.Data
         public DbSet<Tag> Tags { get; set; }
         public DbSet<BlogPostLike> BlogPostLike { get; set; }
         public DbSet<BlogPostComment> BlogPostComment { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Enable retry on failure
+            optionsBuilder.UseSqlServer("Server=tcp:creativorareblogsitedb.database.windows.net,1433;Initial Catalog=CreativoRareBlogSiteDb;Persist Security Info=False;User ID=creativorareblogsitedb;Password=Sixteen@16;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", options =>
+            {
+                options.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
