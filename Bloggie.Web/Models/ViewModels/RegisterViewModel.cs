c390b17f7123ebb82dba3513e45187d807bb8bc8
fad16papa <fad16papa@gmail.com>
2024-03-01 01:28:32 +0800

    Commit changes

--- Bloggie.Web/Models/ViewModels/RegisterViewModel.cs
+++ Bloggie.Web/Models/ViewModels/RegisterViewModel.cs
@@ -1,19 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Bloggie.Web.Utils;
 
 namespace Bloggie.Web.Models.ViewModels
 {
     public class RegisterViewModel
     {
         [Required]
+        [UniqueUsername(ErrorMessage = "Username is already in use.")]
+        [UsernameValidation(ErrorMessage = "Invalid username format.")]
+        [MinLength(8, ErrorMessage = "Username 8 minimum characters allowed")]
+        [MaxLength(20, ErrorMessage = "Username 20 maximum characters allowed")]
         public string Username { get; set; }
 
         [Required]
         [EmailAddress]
+        [UniqueEmail(ErrorMessage = "Email is already in use.")]
         public string Email { get; set; }
 
         [Required]
-        [MinLength(6, ErrorMessage = "Password has a minumum of 6 characters")]
-        [MaxLength(12, ErrorMessage = "Password has a maximum of 20 characters")]
+        [MinLength(8, ErrorMessage = "Password has a minumum of 8 characters")]
+        [MaxLength(30, ErrorMessage = "Password has a maximum of 30 characters")]
         public string Password { get; set; }
     }
 }
