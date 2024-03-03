17742ae3ed27a3f2554cd628601e09ae2b071a43
fad16papa <fad16papa@gmail.com>
2024-03-03 19:21:07 +0800

    Commit changes

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 var builder = WebApplication.CreateBuilder(args);
 
