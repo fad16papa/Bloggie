a2dac188c4bdc68bdd7cfa6beab0ce7b18552751
fad16papa <fad16papa@gmail.com>
2024-04-10 19:40:07 +0800

    Add spinner in each user when its deleted

--- Bloggie.Web/wwwroot/js/package/adminUsers/deleteUser.js
+++ Bloggie.Web/wwwroot/js/package/adminUsers/deleteUser.js
@@ -2,45 +2,53 @@
 $("#deleteUserForm").submit(function (event) {
   event.preventDefault(); // Prevent default form submission
 
+  var form = $(this); // Reference to the form that was submitted
+
   // Disable submit button to prevent multiple clicks
-  $("#btnDeleteUser").prop("disabled", true);
+  form.find("#btnDeleteUser").prop("disabled", true);
 
   // Show loading spinner
-  $("#btnDeleteUser .spinner-border").removeClass("d-none");
+  form.find(".spinner-border").removeClass("d-none");
 
   // Check if a submission is already in progress
-  if ($(this).data("isSubmitting")) {
+  if (form.data("isSubmitting")) {
     // If a submission is in progress, return without making a new request
     return;
   }
+
+  // Get the anti-forgery token value from the cookie
+  var antiForgeryToken = $('input[name="__RequestVerificationToken"]').val();
+
   // Set flag to indicate submission is in progress
-  $(this).data("isSubmitting", true);
+  form.data("isSubmitting", true);
 
   // Serialize the form data
-  var id = $("#userId").text();
+  var id = form.find("#userId").text();
 
   // Make AJAX request
-  $.ajax({
-    url: "/AdminUsers/Delete",
-    type: "POST",
-    headers: {
-      RequestVerificationToken: antiForgeryToken,
-    },
-    data: {
-      id: id,
-    },
-    success: function (response) {
-      // Handle success
-      $("#btnDeleteUser .spinner-border").addClass("d-none");
-      $("#btnDeleteUser").prop("disabled", false);
-      window.location.href = "/AdminUsers/List";
-    },
-    error: function (xhr, status, error) {
-      // Handle error
-    },
-    complete: function () {
-      // Reset flag and enable submit button
-      $("#deleteUserForm").data("isSubmitting", false);
-    },
-  });
+  setTimeout(function () {
+    $.ajax({
+      url: "/AdminUsers/Delete",
+      type: "POST",
+      headers: {
+        RequestVerificationToken: antiForgeryToken,
+      },
+      data: {
+        id: id,
+      },
+      success: function (response) {
+        // Handle success
+        form.find(".spinner-border").addClass("d-none");
+        form.find("#btnDeleteUser").prop("disabled", false);
+        window.location.href = "/AdminUsers/List";
+      },
+      error: function (xhr, status, error) {
+        // Handle error
+      },
+      complete: function () {
+        // Reset flag and enable submit button
+        form.data("isSubmitting", false);
+      },
+    });
+  }, 2000); //put delay 3 seconds
 });
